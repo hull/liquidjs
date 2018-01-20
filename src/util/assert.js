8683397f6331882d162258abfbc3777d655d8afe
Romain Dardour <romain@hull.io>
2018-01-20 13:58:57 +0100

    don't use es6 template literals so UglifyJS is happy

--- src/util/assert.js
+++ src/util/assert.js
@@ -2,7 +2,7 @@ const AssertionError = require('./error.js').AssertionError
 
 function assert (predicate, message) {
   if (!predicate) {
-    message = message || `expect ${predicate} to be true`
+    message = message || 'expect ' + predicate + ' to be true'
     throw new AssertionError(message)
   }
 }
